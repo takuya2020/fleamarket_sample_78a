f6814ceeabac34ef0a068f12ebe162c6de19d272
takuya <zqiaoben51@gmail.com>
2020-07-06 15:03:18 +0900

    top-haml_sass

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
-    def index
+  #  before_action :set_item_name, except: [:new, :edit]
+    
+  def index
+  end
+
+  def search
+    if params[:keyword].present?
+      @Items = Item.where('item LIKE ?', "%#{params[:keywoed]}%")
+    else
+      @Items = Item.none
     end
+  end
 end
