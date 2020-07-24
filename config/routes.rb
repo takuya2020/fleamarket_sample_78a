957791151ccb24a486dcd5db6659f2beb0a33680
Mart-rock <itsmyrockandrollmart34@gmail.com>
2020-07-24 10:16:34 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
     root to: 'items#index'
     resources :items, only: :show
+    resources :users, only: :show
+    resources :items, only: :index
+    
 
     resources :items, except: :show
     resources :item_images, only: [:new, :create]
