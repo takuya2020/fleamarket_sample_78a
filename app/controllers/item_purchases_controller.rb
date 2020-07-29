6f348340825677630d39ed1f94b9684b1dfc91bf
takuya <zqiaoben51@gmail.com>
2020-07-29 19:10:28 +0900

    item-buy

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,44 +1,43 @@
 class ItemPurchasesController < ApplicationController
 
-#　下記のコメントアウトは商品購入機能の実装時の為に残している。
+  require 'payjp'
 
- # before_action :set_card, only: [:pay]
- # before_action :set_item, only: [:pay]
-
-  def pay_index
-   # @card = @set_card.first
-   # if @card.blank?
-   #   redirect_to controller: "cards", action: "new"
-   # else
-   #   Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-   #   customer = Payjp::Customer.retrieve(@card.customer_id)
-   #   @default_card_information = customer.cards.retrieve(@card.card_id)
-   # end
+  def index
+    @item = Item.find(params[:item_id])
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 
- # def pay
- #   @card = @set_card.first
- #   Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
- #   Payjp::Charge.create(
- #   :amount => @item.price,
- #   :customer => @card.customer_id,
- #   :currency => 'jpy',
- # )
- # redirect_to action: 'done', item_id: @item
- # end
-#
- # def done
- #   ItemPurchases.create(item_id: @item.id, user_id: current_user.id)
- # end
-#
- # private
+  def pay
+    @item = Item.find(params[:item_id])
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :customer => card.customer_id, #顧客ID
+    :currency => 'jpy', #日本円
+  )
+  redirect_to action: 'done' #完了画面に移動
+  end
 
-  #def set_card
-  #  @set_card = Card.where(user_id: current_user.id)
-  #end
+  def done
+    @item = Item.find(params[:item_id])
+    ItemPurchase.create(item_id: @item.id, user_id: current_user.id)
+  end
 
- # def set_item
- #   @item = Item.find(params[:item_id])
- # end
-
+  private
+  
+  def item_purchase_params
+    params.require(:item_purchase).permit(:item, :user)
+  end
 end
