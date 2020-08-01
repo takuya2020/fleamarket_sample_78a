eafe1415f33feca7b3ad090cc024e2fe485298d5
Mart-rock <itsmyrockandrollmart34@gmail.com>
2020-08-01 21:51:31 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+    item.destroy
+    end
   end
 
   def show
